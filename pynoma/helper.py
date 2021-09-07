be487aea38814e94fb565b20115f17d621aa1a62
colombelli <xcolombelli@gmail.com>
2021-09-07 15:59:05 -0300

    Add batch_search fix for lots of gnomad requests

--- pynoma/helper.py
+++ pynoma/helper.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
 import pandas as pd
 
+from random import uniform
+from time import sleep
+
 
 # path: "/my/saving/path/plot.png"
 def annotation_barplot(df, path=None):
@@ -22,8 +25,20 @@ def annotation_barplot(df, path=None):
 # a list of GeneSearch, RegionSearch and/or TranscriptSearch objects
 def batch_search(search_objects, standard=True, additional_population_info=False):
     datasets=[]
-    for obj in search_objects:
-        obj_df, _ = obj.get_data(standard=standard, additional_population_info=additional_population_info) 
-        if isinstance(obj_df, pd.DataFrame):
-            datasets.append(obj_df)
+    total_searches = len(search_objects)
+    for i, obj in enumerate(search_objects):
+        print("Batch searching... ", i, "/", total_searches)
+        sleep(uniform(1,5))
+        try:
+            obj_df, _ = obj.get_data(standard=standard, additional_population_info=additional_population_info) 
+            if isinstance(obj_df, pd.DataFrame):
+                datasets.append(obj_df)
+        except Exception as e:
+            if type(e).__name__ == 'KeyError':
+                sleep(30)
+                obj_df, _ = obj.get_data(standard=standard, additional_population_info=additional_population_info) 
+                if isinstance(obj_df, pd.DataFrame):
+                    datasets.append(obj_df)
+            else:
+                raise(e)
     return pd.concat(datasets)#.fillna(0)
