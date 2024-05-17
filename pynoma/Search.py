9b1e79d7004e48aee8f1cc529b95c8a7fb258582
Felipe Colombelli <fcolombelli@inf.ufrgs.br>
2024-05-17 21:12:52 +0200

    Add documentation and type-hinting to Search and RegionSearch; Add dataset_id_map new logic

--- pynoma/Search.py
+++ pynoma/Search.py
@@ -1,68 +1,129 @@
+"""This module contains the Search class, which is used to search the gnomAD database."""
+from typing import Union
 from requests import post
+import pandas as pd
 from pynoma.DataManager import DataManager
 from pynoma.Logger import Logger
 
 class Search:
 
-    # dataset_version: either 3 or 2
-    def __init__(self, dataset_version:int, query, query_variables):
+
+    dataset_id_map = {
+        2: "gnomad_r2_1",
+        "2": "gnomad_r2_1",
+        "hg19": "gnomad_r2_1",
+        "hg37": "gnomad_r2_1",
+        3: "gnomad_r3",
+        "3": "gnomad_r3",
+        "hg38": "gnomad_r3"
+    }
+
+    def __init__(self, dataset_version: Union[int, str], query: str, query_variables: str):
+        """Constructor for the Search class.
+
+        Args:
+            dataset_version: The version of the gnomAD dataset to be used. It can be either 2, or hg19/h38
+            query: The query to be used to search the gnomAD database.
+            query_variables: The variables to be used in the query.
+        """
         self.end_point = "https://gnomad.broadinstitute.org/api/"
         self.query = query
         self.query_vars = query_variables
-        self.reference_genome = None
-        self.dataset_id = self.get_dataset_id(dataset_version)
-        
+        self.dataset_id, self.reference_genome = self.get_dataset_id(dataset_version)
 
         self.dm = None   # attribute holding DataManager object
 
     
-    # variables: a tuple (var1, var2, ..., varn)
-    def request_gnomad(self, variables):
+    def request_gnomad(self, variables: Union[str, tuple]) -> dict:
+        """Send a POST request to the gnomAD API.
+
+        Args:
+            variables: The variables to be used in the query. See examples in the Queries file.
+
+        Returns:
+            The response JSON from the gnomAD API request.
+        """
         variables = self.query_vars % variables
         response = post(self.end_point, data={'query': self.query, 'variables': variables}, timeout=None)
         return response.json()
 
     
-    def get_dataset_id(self, version):
-        if version == 3:
-            self.reference_genome = "GRCh38"
-            return "gnomad_r3"
-        elif version == 2:
-            self.reference_genome = "GRCh37"
-            return "gnomad_r2_1"
+    @classmethod
+    def get_dataset_id(cls, version: Union[int, str]):
+        """Get the dataset ID based on the user version provided.
+
+        The user can provide the gnomAD version as an integer (2 or 3) or the reference genome as a string ("hg19" or
+        "hg38"). For now, the only available versions are gnomAD's version 2 and 3.
+
+        Args:
+            version: The version of the gnomAD dataset to be used. It can be either 2, 3 or hg19/h38
+
+        Raises:
+            Exception: If the version provided is not valid.
+        
+        Returns:
+            The dataset ID and the reference genome.
+        """
+        if version in cls.dataset_id_map:
+            dataset_id = cls.dataset_id_map[version]
+            if dataset_id == "gnomad_r2_1":
+                reference_genome = "GRCh37"
+            else:
+                reference_genome = "GRCh38"
         else:
-            raise Exception('Invalid dataset version. Choose either 2 or 3.')
-        return
+            raise Exception("Invalid dataset version. Choose gnomAD's version 2, 3 or hg19/h38.")
+        return dataset_id, reference_genome
 
 
 class RegionSearch(Search):
 
-    # dataset_version: either 3 or 2
-    def __init__(self, dataset_version:int, chromosome, start_position, end_position):
-
+    def __init__(self, 
+                 dataset_version: Union[int, str],
+                 chromosome: Union[int, str], 
+                 start_position: Union[int, str], 
+                 end_position: Union[int, str]):
+        """Constructor for the RegionSearch class.
+        
+        Args:
+            dataset_version: The version of the gnomAD dataset to be used. It can be either 2, 3 or hg19/h38
+            chromosome: The chromosome number to search for.
+            start_position: The start position of the region to search for.
+            end_position: The end position of the region to search for.
+        """
         from pynoma.Queries import in_region_v3, in_region_v2, in_region_variables
-        if dataset_version == 2:
-            in_region = in_region_v2
+        super().__init__(dataset_version, "", in_region_variables)
+        dataset_id, _ = self.get_dataset_id(dataset_version)
+        if dataset_id == "gnomad_r2_1":
+            self.query = in_region_v2
         else:
-            in_region = in_region_v3
-        super().__init__(dataset_version, in_region, in_region_variables)
+            self.query = in_region_v3
 
         self.chromosome = str(chromosome)
         self.start = str(start_position)
         self.end = str(end_position)
 
 
-    def get_json(self):
+    def get_json(self) -> dict:
+        """Get the JSON data from the gnomAD API."""
         variables = (self.chromosome, self.dataset_id, self.reference_genome, self.start, self.end)
         return self.request_gnomad(variables)
 
+    
+    def get_data(self, 
+                 standard=True, 
+                 additional_population_info=False
+                 ) -> tuple[Union[pd.DataFrame, None], Union[pd.DataFrame, None]]:
+        """Get the data from the gnomAD API.
 
-    # If standard is set to False, it will return everything without processing 
-    # farther than {json to pandas DF}
-    # If additional_population_info is set to True, 9 additional columns will 
-    # be added to the returned dataframe (the 9 population allele frequency for each variant)
-    def get_data(self, standard=True, additional_population_info=False):
+        Args:
+            standard: If True, the data will be processed and returned in a standard format. Defaults to True.
+            additional_population_info: If True, 9 additional columns with population information will be added to the
+                dataframe. This information includes the allele frequency for each variant in 9 different populations.
+                Defaults to False.
 
+        Returns:
+            A tuple containing the dataframe with the data and the clinical dataframe.
+        """
         json_data = self.get_json()
         if not json_data['data']['region']['variants']:
             Logger.no_variants_found()
@@ -74,7 +135,7 @@ class RegionSearch(Search):
             self.dm.process_standard_dataframe()
             if additional_population_info:
                 return self.dm.get_additional_pop_info_df('standard'), self.dm.clinical_df
-            return self.dm.standard_df, self.dm.clinical_df
+            return self.dm.standard_df, self.dm.clinical_df  # TODO: investigate linter type-checking complaint
         
         else:
             if additional_population_info:
@@ -86,7 +147,7 @@ class RegionSearch(Search):
 
 class GeneSearch(Search):
 
-    def __init__(self, dataset_version:int, gene: str):
+    def __init__(self, dataset_version: Union[int, str], gene: str):
 
         from pynoma.Queries import gene_id, gene_id_variables
         super().__init__(dataset_version, gene_id, gene_id_variables)
