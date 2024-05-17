847c30f28350139564372a71d335b3d0d2d1f2b7
Felipe Colombelli <fcolombelli@inf.ufrgs.br>
2024-05-17 22:36:51 +0200

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'Pynoma',         
   packages = ['pynoma'],  
-  version = '0.1.0',
+  version = '0.2.0',
   license='GNU General Public License v3.0',
   description = 'A Python API to communicate with gnomAD database.',   
   author = 'Felipe Colombelli, Paola Carneiro',                   
